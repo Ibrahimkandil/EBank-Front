51a17631cbfb1c8f8dc78f8fe1ec0eb4e4b996f8
mayssem <maysem.benhamouda@istic.ucar.tn>
2024-06-20 17:39:31 +0100

    mayssem wallet 20/06/2024

--- src/app/mayssem/WalletDashboard/header-stats/header-stats.component.ts
+++ src/app/mayssem/WalletDashboard/header-stats/header-stats.component.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from "@angular/core";
+import {WalletDashboardService} from "../wallet-dashboard.service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+
 
 @Component({
   selector: "app-header-stats",
   templateUrl: "./header-stats.component.html",
 })
 export class HeaderStatsComponent implements OnInit {
-  constructor() {}
+  datasources:any[] = [];
+  constructor(private  walletDashboardService:WalletDashboardService, private snackBar: MatSnackBar,) {
+    this.walletDashboardService.fetchWallets().subscribe((data: any) => {
+      this.snackBar.open("Success"), 'Error', {
+        duration: 5000, // duration in milliseconds (optional)
+      };
+      if(data){
+        this.datasources=data;
+      }else{
+        this.datasources=this.walletDashboardService.datasources;
+      }
+    }, (error: any) => {
+    });
+
+  }
 
   ngOnInit(): void {}
 }
