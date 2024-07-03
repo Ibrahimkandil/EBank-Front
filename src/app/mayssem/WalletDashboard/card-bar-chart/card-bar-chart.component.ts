d481954e7a202cb391d00d2e429e5e110ab72033
mayssem <maysem.benhamouda@istic.ucar>
2024-07-03 13:05:47 +0100

    post wallet

--- src/app/mayssem/WalletDashboard/card-bar-chart/card-bar-chart.component.ts
+++ src/app/mayssem/WalletDashboard/card-bar-chart/card-bar-chart.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit, AfterViewInit } from "@angular/core";
 import { Chart } from 'chart.js'
 import {FormBuilder, FormGroup} from "@angular/forms";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {WalletDashboardService} from "../wallet-dashboard.service";
+import {CookieService} from "ngx-cookie-service";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Observable} from "rxjs";
 
 @Component({
   selector: "app-card-bar-chart",
   templateUrl: "./card-bar-chart.component.html",
 })
 export class CardBarChartComponent implements OnInit, AfterViewInit {
+  rates:any=[]
   form: FormGroup;
   options1: string[] = [];
   options2: string[] = [];
@@ -16,13 +20,26 @@ export class CardBarChartComponent implements OnInit, AfterViewInit {
   currencies = ['USD', 'EUR', 'JPY', 'GBP', 'AUD', 'CAD', 'CHF', 'CNY', 'SEK', 'NZD'];
   mode = ['Buy', 'Sell'];
 
-  constructor(private fb: FormBuilder,private http:HttpClient,private walletDashboardService: WalletDashboardService ) {
+  constructor(      private cookieservice:CookieService,
+                    private snackBar: MatSnackBar,private fb: FormBuilder,private http:HttpClient,private walletDashboardService: WalletDashboardService ) {
     this.form = this.fb.group({
       dropdown1: [''],
       dropdown2: [''],
       dropdown3: [''],
-      input1: ['']
+      input1: [0]
     });
+    this.GetCompteBancaire()
+    this.getExchangeRates().subscribe((res:any)=>
+    {
+      this.rates=res.rates
+      console.log("res",this.rates)
+      this.selectedRate=this.rates['USD']
+
+
+    },
+      (err:any)=>{
+
+      })
   }
   historicalData: any = [];
 
@@ -35,13 +52,57 @@ export class CardBarChartComponent implements OnInit, AfterViewInit {
     }
 
   }
+  comptes:any;
+  listcomptes:any=[]
+  GetCompteBancaire(){
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer '+this.cookieservice.get('token')
+    });
+    this.http.get('http://localhost:8081/ebank/api/v1/client/comptes/'+this.cookieservice.get('id'), {headers: headers}).
+    subscribe((resp: any) => {
+      this.comptes=resp;
+      for(let i =0;i<this.comptes.length;i++){
+        this.listcomptes[i]=this.comptes[i].account_number
+      }
+      console.log("this.comptes",this.comptes)
+      console.log("this.comptes.length",this.comptes.length)
+      console.log("this.comptes.account_number",this.comptes[0].account_number)
+      console.log("this.listcomptes",this.listcomptes)
+
+  })
+  }
+  datas_CompteBancaire:any=null
   h1(e:any) {
-    let findByCurrency = this.historicalData.find((element: any) => element.currency === e);
+      this.datas_CompteBancaire= this.comptes.find((element: any) => element.account_number === e);
     console.log("e", e)
+    console.log("this.Co,pt",this.datas_CompteBancaire)
   }
   onSubmit(): void {
-    console.log(this.form.value);
+
+    console.log("this.task",this.task)
+    console.log("this.task",this.task)
+    if(this.task==="CREATE" && this.objWallet===null ){
+      if(this.form.get('input1')?.value!=0){
+      let body={
+
+        "currency":this.cuurency,
+
+         "id_client":this.cookieservice.get("id"),
+         "double balance":this.form.get('input1')?.value
+      }
+        this.http.post('http://localhost:8081/ebank/api/v1/client/wallets/'+this.datas_CompteBancaire.id,body, {headers: this.headers}).
+        subscribe((resp: any) => {},(err:any)=>{})
+
+      }
+
+    }
+    // console.log(this.form.value);
   }
+  getExchangeRates(): Observable<any> {
+    let url=`https://api.exchangerate-api.com/v4/latest/TND`
+    return this.http.get<any>(url);
+  }
+
   ngAfterViewInit() {
     let config:any = {
       type: "bar",
@@ -139,5 +200,66 @@ export class CardBarChartComponent implements OnInit, AfterViewInit {
     ctx = ctx.getContext("2d");
     new Chart(ctx, config);
   }
+  rate_Init=this.rates[`${this.currencies[0]}`]
 
+
+
+
+
+
+
+
+
+
+task=""
+  objWallet:any=null
+   headers = new HttpHeaders({
+    'Authorization': 'Bearer '+this.cookieservice.get('token')
+  });
+  selectedRate:any
+  cuurency:any="USD"
+  h2(e:any) {
+    console.log("e", this.rates[`${e}`])
+    this.cuurency=e
+    this.selectedRate=this.rates[`${e}`]
+    let body={
+      "currency":e,
+      "id_client":parseInt(this.cookieservice.get('id'))
+
+    }
+
+    this.http.post('http://localhost:8081/ebank/api/v1/client/wallets/Bycurrency',body, {headers: this.headers}).
+    subscribe((resp: any) => {
+      if(resp==="CREATE"){
+        this.task=resp
+      }else{
+        this.objWallet=resp
+      }
+    },(err:any)=>{})
+  }
+  valueInDinar:any=0
+  supthanbalance:boolean=false
+  meth:any="Buy"
+
+  h0(e:any){
+ this.meth=e
+  }
+  h3(e:any){
+    console.log('selectedRate',this.selectedRate)
+    if(this.meth==="Buy"){
+
+      this.valueInDinar=this.form.get('input1')?.value/this.selectedRate
+    }else{
+      this.valueInDinar=this.form.get('input1')?.value/this.selectedRate
+
+    }
+
+    if(this.valueInDinar>this.datas_CompteBancaire.balance) {
+      this.supthanbalance=true
+    }else{
+      this.supthanbalance=false
+    }
+    console.log(e)
+    console.log(this.form.get('input1')?.value)
+  }
 }
